eb55b4d96b7c42e42f1c21311d20f328fe554cf5
mondie <richmondagorvor@gmail.com>
2020-11-03 02:12:02 +0000

    my first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function temperature(finalTemp, initialTemp){
+    return finalTemp - initialTemp
+   
+}
+
+
+temperature(50,10)
+
+function addition(x,y){
+    return x+y;
+
+}
+
+addition(20,4)
